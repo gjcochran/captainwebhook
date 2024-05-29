11c5eda5a111762509cd7090cbfaf5315c546da1
Garrett Cochran <garrettcochran@Garretts-MacBook-Pro.local>
2024-05-29 11:05:54 -0600

    refactor: changed route middleware to handle all http requests from use to all. express.all() seems more semantically accurate as well as more clear

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
-import { createNewBinId, addToMongo, addToPostgres } from './helpers';
+import express from "express";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+import { createNewBinId, addToMongo, addToPostgres } from "./helpers";
 
 const app = express();
 const port = 3000;
@@ -9,17 +9,23 @@ const port = 3000;
 app.use(express.json());
 
 app.use((req: any, _, next) => {
-  console.log('Subdomain:', req.headers.host.split('.')[0]);
+  console.log("Subdomain:", req.headers.host.split(".")[0]);
   next();
 });
 
-// // Middleware to handle all request methods for the same path
-app.use('/bin/:bin_id', async (req, res) => {
+// was middleware, now express.all() for clarity
+app.all("/bin/:bin_id", async (req, res) => {
   const { method, url, headers, query, body } = req;
   console.log({ method, url, headers, query, body });
   const mongoRequestId = await addToMongo(req);
-  const requestId = uuidv4()
-  await addToPostgres(method, url, mongoRequestId, req.params.bin_id, requestId);
+  const requestId = uuidv4();
+  await addToPostgres(
+    method,
+    url,
+    mongoRequestId,
+    req.params.bin_id,
+    requestId,
+  );
 
   res.send(`Handled ${req.method} request`);
   return mongoRequestId;
@@ -27,20 +33,20 @@ app.use('/bin/:bin_id', async (req, res) => {
 
 // To serve public directory for the path '/public/bins/:bin_id'
 app.use(
-  '/public/bin/:bin_id',
-  express.static(path.join(__dirname, '../client/public')),
+  "/public/bin/:bin_id",
+  express.static(path.join(__dirname, "../client/public")),
 );
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
   console.log(`Method: ${req.method}`);
   console.log(`URL: ${req.url}`);
-  console.log('Headers:', req.headers);
-  console.log('Query Params:', req.query);
-  console.log('Body:', req.body);
+  console.log("Headers:", req.headers);
+  console.log("Query Params:", req.query);
+  console.log("Body:", req.body);
 });
 
-app.post('/api/create_new_bin', async (_, res) => {
+app.post("/api/create_new_bin", async (_, res) => {
   // create new bin id
   // add check to ensure not a duplicate
   // store bin id in postgres
@@ -52,17 +58,17 @@ app.post('/api/create_new_bin', async (_, res) => {
 
 // API
 // Get info about specific request
-app.get('/api/:bin_id/requests/:request_id', () => ({}));
+app.get("/api/:bin_id/requests/:request_id", () => ({}));
 
-app.get('/api/:bin_id', (_, res) => {
+app.get("/api/:bin_id", (_, res) => {
   // logic to get all requests for a specific binId
   // interacting with postgres to select all requests from request where requestbin_id == binId
-  console.log('path is /api/:bin_id');
+  console.log("path is /api/:bin_id");
   const requestId = uuidv4();
   res.json([
-    { path: 'google.com', method: 'POST', time: '3:00PM', id: requestId },
-    { path: 'stuff.com', method: 'GET', time: '12:00PM', id: requestId },
-    { path: 'not.com', method: 'POST', time: '5:00AM', id: requestId },
+    { path: "google.com", method: "POST", time: "3:00PM", id: requestId },
+    { path: "stuff.com", method: "GET", time: "12:00PM", id: requestId },
+    { path: "not.com", method: "POST", time: "5:00AM", id: requestId },
   ]);
 });
 
